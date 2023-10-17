b40b225ccec6e8e42ee8713bd57a9c380a87589d
lucas <lucas.lima716@etec.sp.gov.br>
2023-10-17 20:36:17 -0300

    alo

--- bhb/jscri.js
+++ bhb/jscri.js
@@ -13,13 +13,25 @@ alert(`Seja bem vindo ${nome2} `); */
 let botao1=document.querySelector('#btn1');
 botao1.addEventListener('click',()=>{
     let nome=document.querySelector('#nome');
-    let texto_nome=nome.value;
+    let texto_nome=Boolean (nome.value);
     let num1=document.querySelector('#num1');
     let num2=document.querySelector('#num2');
-    let valor1=num1.value;
-    let valor2=num2.value;
+    let valor1=Number (num1.value);
+    /*alert(typeof valor1);
+    valor1=String (valor1);
+    alert(typeof valor1);
+    const valor2=3; constante*/
+
+    let valor2=Number (num2.value);
+    
+    //alert(valor1 ** valor2)
+    //alert("a"*2); isso é um erro
+
+    let pot=valor1**valor2;
+
     let p=document.querySelector(".p1");
     p.textContent=`o nome é ${texto_nome}, o primeiro número: ${valor1} e o segundo número é: ${valor2}`;
+    p.textContent+=`A potencia deu: ${pot}`
 
 
     //alert(`o nome é ${texto_nome}, o primeiro número: ${valor1} e o segundo número é: ${valor2}`);
